99201dec4aea3f541b733ada1d0952f3f37ae570
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-04-21 16:46:11 +0200

    Level up to a released version of the countdown jar.

--- src/uk/co/eelpieconsulting/countdown/android/CountdownActivity.java
+++ src/uk/co/eelpieconsulting/countdown/android/CountdownActivity.java
@@ -1,15 +1,10 @@
 package uk.co.eelpieconsulting.countdown.android;
 
-import uk.co.eelpieconsulting.countdown.api.CountdownApi;
-import uk.co.eelpieconsulting.countdown.model.StopBoard;
-import uk.co.eelpieconsulting.countdown.parsers.StopBoardParser;
-import uk.co.eelpieconsulting.countdown.urls.CountdownApiUrlBuilder;
-import uk.co.eelpieconsulting.countdown.util.HttpFetcher;
-
-import uk.co.eelpieconsulting.countdown.android.R;
 import uk.co.eelpieconsulting.countdown.android.daos.FavouriteStopsDAO;
-
-
+import uk.co.eelpieconsulting.countdown.api.CountdownApi;
+import uk.co.eelpieconsulting.countdown.exceptions.HttpFetchException;
+import uk.co.eelpieconsulting.countdown.exceptions.ParsingException;
+import uk.co.eelpieconsulting.countdown.model.StopBoard;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -24,22 +19,32 @@ public class CountdownActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        CountdownApiUrlBuilder countdownApiUrlBuilder = new CountdownApiUrlBuilder("http://countdown.tfl.gov.uk");
-        HttpFetcher httpFetcher = new HttpFetcher();
-        StopBoardParser stopBoardParser = new StopBoardParser();
-        api = new CountdownApi(countdownApiUrlBuilder, httpFetcher, stopBoardParser);
+        api = new CountdownApi("http://countdown.tfl.gov.uk");
         favouriteStopsDAO = new FavouriteStopsDAO();
     }
     
 	@Override
 	protected void onResume() {
 		super.onResume();
-		final StopBoard stopboard = loadArrivals();
+
 		final TextView arrivalsTextView = (TextView) findViewById(R.id.arrivals);
-		arrivalsTextView.setText(stopboard.getArrivals().toString());        
+		try {
+			StopBoard stopboard = loadArrivals();
+			arrivalsTextView.setText(stopboard.getArrivals().toString());
+			return;
+			
+		} catch (HttpFetchException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParsingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		arrivalsTextView.setText("Failed to load arrivals");
 	}
 	
-	private StopBoard loadArrivals() {
+	private StopBoard loadArrivals() throws HttpFetchException, ParsingException {
 		return api.getStopBoard(favouriteStopsDAO.getFavouriteStop());
 	}
 	
