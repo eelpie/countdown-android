d12b5442af83635ed04b7cba284c1accb648beb4
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-08-03 19:54:50 +0100

    Clean up formatting of alerts screen.

--- src/uk/co/eelpieconsulting/countdown/android/views/MessageDescriptionService.java
+++ src/uk/co/eelpieconsulting/countdown/android/views/MessageDescriptionService.java
@@ -1,19 +1,29 @@
 package uk.co.eelpieconsulting.countdown.android.views;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
 import uk.co.eelpieconsulting.busroutes.model.Message;
 import uk.co.eelpieconsulting.busroutes.model.MultiStopMessage;
 import uk.co.eelpieconsulting.busroutes.model.Stop;
+import uk.co.eelpieconsulting.countdown.android.services.StopNameComparator;
+
 import android.content.Context;
 import android.view.View;
 import android.widget.TextView;
 
 public class MessageDescriptionService {
 
+	private static final String MESSAGE_START_DATE_FORMAT = "dd MMMM, kk:mm a";
+	
+	private static DateFormat dateFormatter = new SimpleDateFormat(MESSAGE_START_DATE_FORMAT);
 
+	private static StopNameComparator stopNameComparator = new StopNameComparator();
+	
 	public static TextView makeStopDescription(Message message, Context context) {
 		return makeMessageView(message, context, new ArrayList<Stop>());
 	}
@@ -23,15 +33,30 @@ public class MessageDescriptionService {
 	}
 	
 	private static TextView makeMessageView(Message message, Context context, List<Stop> stops) {
-		final StringBuilder output = new StringBuilder(message.getMessage() + "\n");
-		output.append(new Date(message.getStartDate()));
-		for (Stop stop : stops) {
-			output.append(stop.getName() + ", ");
-		}
+		final StringBuilder output = new StringBuilder();
+		output.append(dateFormatter.format(new Date(message.getStartDate())));
+		output.append(": ");
+		output.append(message.getMessage());
+		output.append("\n");
+		appendCommaSeperatedStopNames(stops, output);
+		
 		TextView messageView = new TextView(context);
 		messageView.setText(output.toString() + "\n\n\n");
 		messageView.setVisibility(View.VISIBLE);
 		return messageView;		
+	}
+
+	private static void appendCommaSeperatedStopNames(List<Stop> stops, final StringBuilder output) {
+		Collections.sort(stops, stopNameComparator);
+		
+		int i = 1;		
+		for (Stop stop : stops) {
+			output.append(StopDescriptionService.makeStopTitle(stop));
+			if (i < stops.size()) {
+				output.append(", ");
+			}
+			i++;
+		}
 	}
 	
 }
